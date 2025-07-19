5a9822c83241512b179bdf4aa69b97684ebff32e
i586592 <snir.taoub@sap.com>
2025-07-19 20:08:59 +0300

    add try catch mechanism to kafka publisher

--- src/events/publisher.js
+++ src/events/publisher.js
@@ -6,10 +6,14 @@ const producer = kafka.producer();
 (async () => await producer.connect())();
 
 async function publishEvent(topic, data) {
-    await producer.send({
-        topic,
-        messages: [{ value: JSON.stringify(data) }],
-    });
+    try {
+        await producer.send({
+            topic,
+            messages: [{ value: JSON.stringify(data) }],
+        });
+    } catch (err) {
+        console.error(`[Kafka] Failed to publish event to ${topic}:`, err);
+    }
 }
 
 module.exports = { publishEvent };
